ab53beea276914c828854c9f8d58e21d93eecf1c
Jesus Rodriguez-Luna <112538272+JRodriguez-Luna@users.noreply.github.com>
2025-07-28 06:14:35 -0700

    Fix size in SideNav and Dashboard. ( #16 )

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -33,7 +33,7 @@ export function SideNav() {
     return (
         <>
             {/* Container */}
-            <div className="flex flex-col fixed max-sm:hidden w-auto h-dvh shadow-lg">
+            <div className="flex flex-col fixed max-sm:hidden w-auto h-dvh shadow-lg text-[0.75rem]">
                 {/* title display */}
                 <div className="flex w-full h-15 justify-center items-center border-b border-b-slate-200 p-5">
                     <div className="text-3xl text-blue-500">Leasely</div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ import './Dashboard.css'
 
 export function Dashboard() {
     return (
-        <main className='dashboard-container h-full flex flex-col sm:ml-46 px-5 py-5 max-sm:mb-22 gap-8'>
+        <main className='dashboard-container h-full flex flex-col sm:ml-40 px-5 py-5 max-sm:mb-22 gap-8'>
             <Header />
             <WelcomeBack />
             <SummaryCard />
